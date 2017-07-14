ab79894205d068f8e24121915b3c79aa56e940e0
Alex Fuser <alexfuser@mozilla-mexico.org>
2017-07-14 01:39:16 -0500

    New website look

--- js/script.js
+++ js/script.js
@@ -1,17 +1,6 @@
-
-$( document ).ready(function(){
-var current = 1; //keeps track of the current div
-var height = $('.roles').height(); //the height of the roles div
-var numberDivs = $('.roles').children().length; //the number of children of the roles div
-var first = $('.roles div:nth-child(1)'); //the first div nested in roles div
-setInterval(function() {
-    var number = current * -height;
-    first.css('margin-top', number + 'px');
-    if (current === numberDivs) {
-        first.css('margin-top', '0px');
-        current = 1;
-    } else current++;
-}, 2000);
+/*----------------------------------------------------*/
+/* Quote Loop
+------------------------------------------------------ */
 
 function fade($ele) {
     $ele.fadeIn(1000).delay(3000).fadeOut(1000, function() {
@@ -75,8 +64,3 @@ jQuery(document).ready(function($) {
 
 
 TweenMax.staggerFrom(".heading", 0.8, {opacity: 0, y: 20, delay: 0.2}, 0.4);
-
-
-});
-
-
